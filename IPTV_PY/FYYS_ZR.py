b134fee99edecfb3365a4fb11fbffdf5b0ca0aae
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-11 04:34:47 +0800

    Add files via upload

--- IPTV_PY/FYYS_ZR.py
+++ IPTV_PY/FYYS_ZR.py
@@ -4,6 +4,7 @@ import requests, re
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
+
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
 }
@@ -54,12 +55,15 @@ def get_urlid(movie_id):
 def get_m3u8_url(movie_id, urlid):
     url = f"{host}api/api/tv.movie/url"
     params = {
-        "movid": "3849",
-        "urlid": "65913",
+        "movid": movie_id,
+        "urlid": urlid,
         "type": "1",
         "lang": "zh"
     }
     response = requests.get(url, headers=headers, params=params).json()
+    if response['msg'] == 'url not find':
+        params['type'] = '2'
+        response = requests.get(url, headers=headers, params=params).json()
     json_data = response["data"]
     # print(json_data)
     return json_data["url"]
@@ -73,18 +77,15 @@ def get_church_all_url(church):
         movie_js = {}
         movie_id, movie_title = movie['movid'], movie['title']
         urlid_info = get_urlid(movie_id)
-        pool_2 = ThreadPoolExecutor(10)
+        pool_2 = ThreadPoolExecutor(15)
         for info in urlid_info:
             urlid, title, = info["urlid"], info["title"]
             title = str(title).strip()
             result = pool_2.submit(get_m3u8_url, movie_id, urlid)
-            # m3u8_url = get_m3u8_url(movie_id, urlid)
             m3u8_url = result.result()
             # print(m3u8_url)
             movie_js[title] = m3u8_url
         pool_2.shutdown()
-        # print(movie_js)
-        # print(movie_title)
         if year in movie_title:
             movie_js = dict(sorted(movie_js.items(), key=lambda x: x[0], reverse=True))
         # print(movie_js)
@@ -144,7 +145,7 @@ def check_change(church):
 def update():
     global fy_ys
     with open('../SJ_JSON/福音影视.json', 'r', encoding='utf-8') as f:
-        old_js = f.read().replace("'", '"')
+        old_js = f.read().replace("'", '"').replace('"s_', "'s_")
         fy_ys = json.loads(old_js)
         # print(fy_ys)
 
