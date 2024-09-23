339db8b4a460ed7803b22f5526968a6d0ac46889
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-24 00:01:18 +0800

    Update FT_xyys.py

--- IPTV_PY/FT_xyys.py
+++ IPTV_PY/FT_xyys.py
@@ -73,8 +73,8 @@ def get_m3u8(data):
                 _url = match.replace('\\x26amp;', '&').replace('http', 'https')
                 m3u8_url = f'{_url}&vid={video_id}&format_id=10002&support_redirect=0&mmversion=false'
                 res = requests.get(m3u8_url, headers=_headers)
-                # print(res,m3u8_url)
-                print(res.conrent)
+                print(res, m3u8_url)
+                print(res.content)
                 m3u8_urls.append(m3u8_url)
                 if "Chosen" in title and index % 4 == 3:
                     list_xyys[f"{title}-{int(index / 4) + 1}"] = m3u8_urls
