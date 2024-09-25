af03e3bb937db965d2b0be2e758b3303eeb4ccf8
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-25 16:38:44 +0800

    Update demo.js

--- YS_JS/demo.js
+++ YS_JS/demo.js
@@ -1,5 +1,6 @@
 // 修正：分类改静态 (网站频繁变动分类)
 import { load, _ } from "./lib/cat.js";
+const fs = require('fs');
 let key = "地方直播频道";
 let HOST = 'https://v.iqilu.com/';
 let siteKey = "";
@@ -43,8 +44,22 @@ async function homeVod() {
 }
 
 async function category(tid, pg, filter, extend) {
-    
-    let videos =
+    fs.readFile('LIVE_JSON/sdpd.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  try {
+    console.log(data,typeof data)
+    const jsonData = JSON.parse(data);
+    console.log(jsonData);
+  } catch (err) {
+    console.error('Invalid JSON:', err);
+  }
+});
+    let videos =[]
+    for(i=0; i<jsonData['data'].length;i++)
+        videos.push(jsonData['data'][i])
     return JSON.stringify({
         list: videos,
     });
@@ -64,12 +79,14 @@ async function detail(id) {
 }
 
 async function play(flag, id, flags) {
-    const link = HOST + "/skin/kuqimv/play.php";
-    const ref = HOST + "/play/" + id + ".html";
-    const pdata = { id: id };
-    const playUrl = JSON.parse(await request(link, ref, "post", pdata)).url;
+    // const link = HOST + "/skin/kuqimv/play.php";
+    // const ref = HOST + "/play/" + id + ".html";
+    // const pdata = { id: id };
+    // const playUrl = JSON.parse(await request(link, ref, "post", pdata)).url;
+    const playUrl = "https://3geau1mtagczdnqb3fa5dc.wslivehls.com/clivealone302.iqilu.com/291/caffbd9c4ae445d086cfe94302442d30/playlist.m3u8?auth=38d662c673cca2192c5dd52f6042ff5a&timestamp=1727253195568&wsSession=c6899729c97d72b005fc0ca0-172725319572989&wsIPSercert=3aa22d18ba1a130b780b3966a839dc3b&wsiphost=local&wsBindIP=1"
     const headers = {
         Referer: HOST,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
     };
     return JSON.stringify({
         parse: 0,
