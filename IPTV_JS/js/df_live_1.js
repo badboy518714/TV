54450a473113ed8e15d36285ab7c6bec414a24ae
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-26 02:22:56 +0800

    Create df_live_1.js

---
+++ IPTV_JS/js/df_live_1.js
@@ -0,0 +1,95 @@
+import { load, _ } from '../lib/cat.js';
+let key = '🐰山东';
+let HOST = 'https://www.tuxiaobei.com';
+let siteKey = '';
+let siteType = 0;
+const IOS_UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
+
+
+async function request(reqUrl, agentSp) {
+    let res = await req(reqUrl, {
+        method: 'get',
+        headers: {
+            'User-Agent': agentSp || IOS_UA,
+        },
+    });
+    return res.content
+}
+
+async function init(cfg) {
+    siteKey = cfg.skey;
+    siteType = cfg.stype
+}
+
+async function home(filter) {
+    const classes = [{ type_id: '', type_name: '看电视' },{ type_id: '1', type_name: '听广播' },{ type_id: '2', type_name: '🐰02' }];
+    const filterObj = {};
+    return JSON.stringify({
+        class: _.map(classes, (cls) => {
+            cls.land = 1;
+            cls.ratio = 1.78;
+            return cls;
+        }),
+        filters: filterObj,
+    })
+}
+
+async function homeVod() {
+    return '{}'
+}
+
+async function category(tid, pg, filter, extend) {
+    const url = 'https://badboy518714.github.io/TV/IPTV_TXT/山东_地方.txt'
+    const link = await request(url);
+    const html = link.match(/\((.*?)\);/)[1];
+    const data = JSON.parse(html).data;
+    return JSON.stringify({
+        page: 1,
+        pagecount: 1,
+        limit: 10,
+        total: 10 * 2,
+        list: data,
+    })
+}
+
+async function detail(id) {
+    const vod = {
+        vod_id: id,
+        vod_remarks: '',
+    };
+    const playlist = ['点击播放' + '$' + id];
+    vod.vod_play_from = "道长在线";
+    vod.vod_play_url = playlist.join('#');
+    return JSON.stringify({
+        list: [vod],
+    });
+}
+
+async function play(flag, id, flags) {
+    var playUrl = id;
+    const headers = {
+        Referer: "https://v.iqilu.com/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
+    };
+    return JSON.stringify({
+        parse: 0,
+        url: playUrl,
+        header: headers,
+    });
+}
+
+async function search(wd, quick) {
+    return '{}'
+}
+
+export function __jsEvalReturn() {
+    return {
+        init: init,
+        home: home,
+        homeVod: homeVod,
+        category: category,
+        detail: detail,
+        play: play,
+        search: search,
+    }
+}
