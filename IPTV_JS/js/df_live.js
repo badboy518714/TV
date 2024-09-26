c3a24de6349529d94c9f31b629b60a99ba7767ec
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-27 00:09:07 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -31,7 +31,7 @@ async function init(cfg) {
 }
 
 async function home(filter) {
-    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: 'ces60' }];
+    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: 'ces160' }];
     const filterObj = {};
     return JSON.stringify({
         class: _.map(classes, (cls) => {
@@ -54,19 +54,19 @@ async function category(tid, pg, filter, extend) {
     const html = await request(link, referer);
     const $ = load(html);
     var videos = []
-    // if (tid === ''){
-    //     const items = $("div.dianshi_tv > dl");
-    //     videos = _.map(_.slice(items, 0, 9), (item) => {
-    //         var img = $(item).find("img:first")[0];
-    //         var a = $(item).find('a:first')[0];
-    //         return {
-    //             vod_id: a.attribs.href.replace(/.*?\/live\/(.*)\//g, '$1'),
-    //             vod_name: a.attribs["title"],
-    //             vod_pic: img.attribs["src"],
-    //             vod_remarks: ''
-    //         };
-    //     });
-    // }
+    if (tid === ''){
+        const items = $("div.dianshi_tv > dl");
+        videos = _.map(_.slice(items, 0, 9), (item) => {
+            var img = $(item).find("img:first")[0];
+            var a = $(item).find('a:first')[0];
+            return {
+                vod_id: a.attribs.href.replace(/.*?\/live\/(.*)\//g, '$1'),
+                vod_name: a.attribs["title"],
+                vod_pic: img.attribs["src"],
+                vod_remarks: ''
+            };
+        });
+    }
     // console.log(videos)
     const videos1 = [{
             vod_id: 'sdws',
