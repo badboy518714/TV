9132ba67f515efa9f872b7e31a40a46808a3d3c0
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-26 10:33:14 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -29,7 +29,7 @@ async function init(cfg) {
 }
 
 async function home(filter) {
-    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: 'ces15' }];
+    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: 'ces10' }];
     const filterObj = {};
     return JSON.stringify({
         class: _.map(classes, (cls) => {
@@ -90,15 +90,15 @@ async function play(flag, id, flags) {
     const link = HOST + id;
     const referer = HOST;
     const html = await request(link, referer);
-    let regex = /var _pdCid = "(\d+)"/;
-    let _pdCid = html.match(regex)[1];
-    let _data = get_s(_pdCid);
-    let data = _data["data"];
-    let url = "https://feiying.litenews.cn/api/v1/auth/exchange?t=" +  _data["t"] + "&s=" + _data["s"];
-    const res = await request(url, referer, 'post', data);
-    let _url = get_url(res);
-    let response =  await request(_url, referer);
-    let playUrl = response.match(/(http.*)/)[1]
+    // let regex = /var _pdCid = "(\d+)"/;
+    // let _pdCid = html.match(regex)[1];
+    // let _data = get_s(_pdCid);
+    // let data = _data["data"];
+    // let url = "https://feiying.litenews.cn/api/v1/auth/exchange?t=" +  _data["t"] + "&s=" + _data["s"];
+    // const res = await request(url, referer, 'post', data);
+    // let _url = get_url(res);
+    // let response =  await request(_url, referer);
+    // let playUrl = response.match(/(http.*)/)[1]
 
 
 
