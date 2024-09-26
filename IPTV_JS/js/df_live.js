2a17df687fe7228715d5f8abb5fc9059b2829fca
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-26 10:50:18 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -29,7 +29,7 @@ async function init(cfg) {
 }
 
 async function home(filter) {
-    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: 'ces121' }];
+    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: 'ces21' }];
     const filterObj = {};
     return JSON.stringify({
         class: _.map(classes, (cls) => {
@@ -91,7 +91,7 @@ async function play(flag, id, flags) {
     const referer = HOST;
     const html = await request(link, referer);
     let regex = /var _pdCid = "(\\d+)"/;
-    let _pdCid = html.match(regex)[1];
+    let _pdCid = html.match(regex);
     // let _data = get_s(_pdCid);
     // let data = _data["data"];
     // let url = "https://feiying.litenews.cn/api/v1/auth/exchange?t=" +  _data["t"] + "&s=" + _data["s"];
