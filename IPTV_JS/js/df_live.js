c7c77c862a14fe0350a984f995dc66524d10fbd4
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-26 22:16:26 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -10,7 +10,7 @@ const PC_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHT
 
 async function request(reqUrl, referer, mth, data, hd) {
     const headers = {
-        "User-Agent": PC_UA,
+        "User-Agent": IOS_UA,
     };
     if (referer) headers.referer = encodeURIComponent(referer);
     let res = await fetch(reqUrl, {
