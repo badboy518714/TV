d45c57be3403f63e4edeceb785dc23497393a436
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-27 01:40:57 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -31,7 +31,7 @@ async function init(cfg) {
 }
 
 async function home(filter) {
-    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: '04' }];
+    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: '05' }];
     const filterObj = {};
     return JSON.stringify({
         class: _.map(classes, (cls) => {
@@ -54,7 +54,9 @@ async function category(tid, pg, filter, extend) {
         const html1 = await request(link);
         let _pdCid = html1.match(/var _pdCid = "(\d+)"/)[1];
         let _data = get_s(_pdCid);
-
+         let data = _data["data"];
+        let url = "https://feiying.litenews.cn/api/v1/auth/exchange?t=" +  _data["t"] + "&s=" + _data["s"];
+        const res = await request(url, '', 'post', data);
 
 
 
@@ -70,7 +72,7 @@ async function category(tid, pg, filter, extend) {
                     vod_id: a.attribs.href.replace(/.*?\/live\/(.*)\//g, '$1'),
                     vod_name: a.attribs["title"],
                     vod_pic: img.attribs["src"],
-                    vod_remarks: _pdCid
+                    vod_remarks: _data
                 };
             });
         }
