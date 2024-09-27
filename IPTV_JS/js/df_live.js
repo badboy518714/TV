e337d4e91c7337cad346e4dffe6977fa57ab83be
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-27 13:48:35 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -16,7 +16,7 @@ async function request(reqUrl, referer, mth, data, hd) {
     let res = await req(reqUrl, {
         method: mth || "get",
         headers: headers,
-        body: data,
+        data: data,
         postType: ""
         // postType: mth === "post" ? "form" : "",
     });
