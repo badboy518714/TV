f79f063e9b46be0362913fc2a400f4d323b1d668
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-28 06:50:15 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -7,6 +7,7 @@ let siteKey = '';
 let siteType = 0;
 const IOS_UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
 const PC_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36";
+const Agent = [IOS_UA, IOS_UA, PC_UA]
 const ID_JN = ["新闻频道", "都市频道", "文旅体育频道", "生活频道", "少儿频道", "鲁中频道", "城市电视频道", "地铁电视频道", "移动电视频道", "新闻频率", "经济频率", "交通频率", "音乐频率", "936私家车频率", "都市频率", "生活频率"]
 const PIC_JN = ["https://quehuaobs.ijntv.cn/58CE1F28EF5E42F6/FABU_YUNSHI/388B9096297F45A399F2AA6B9690C30F/B8CB223825D84DBAAC4A20FF87D3AC55.jpg","https://quehuaobs.ijntv.cn/58CE1F28EF5E42F6/FABU_YUNSHI/388B9096297F45A399F2AA6B9690C30F/8D3DDDD03A764180A80BC06EC5F359D0.jpg","https://quehuaobs.ijntv.cn/58CE1F28EF5E42F6/FABU_YUNSHI/0683FD92CC394CB0A45EFC96E3573714/8C4F3D94819445969F7AEA4183046B94.png","https://quehuaobs.ijntv.cn/58CE1F28EF5E42F6/FABU_YUNSHI/1AB422BDF3F14167B3FFBBD3FCE7B83F/308CB37FA01E4ACF91F46DCA51C3C01F.jpg","https://quehuaobs.ijntv.cn/58CE1F28EF5E42F6/FABU_YUNSHI/1AB422BDF3F14167B3FFBBD3FCE7B83F/92374E7275A94CB28CC9D847AAA9E953.jpg","https://quehuaobs.ijntv.cn/58CE1F28EF5E42F6/FABU_YUNSHI/0683FD92CC394CB0A45EFC96E3573714/C7A16701895348BD90C932DC35EE6445.jpg","https://quehuaobs.ijntv.cn/58CE1F28EF5E42F6/FABU_YUNSHI/0683FD92CC394CB0A45EFC96E3573714/F62353C7A24E4A928E36375FDF0199AD.png","https://quehuaobs.ijntv.cn/58CE1F28EF5E42F6/FABU_YUNSHI/0683FD92CC394CB0A45EFC96E3573714/FEBCA467466B45FA854714AF823C2546.png","https://quehuaobs.ijntv.cn/58CE1F28EF5E42F6/FABU_YUNSHI/1AB422BDF3F14167B3FFBBD3FCE7B83F/A1E137EFDE7C4147A8D5D783BBD36432.jpg","https://mp-b77cdf68-e470-49bf-98e7-01d12ec29f28.cdn.bspapp.com/staic-img/tv-redio-back.jpg"]; 
 const ID_QD = ['QTV-1', 'QTV-2', 'QTV-3', 'QTV-4', 'QTV-5', 'QTV-6', '新闻综合广播', '经济广播', '文艺广播', '交通广播', '音体广播', '故事广播']
@@ -38,7 +39,7 @@ async function init(cfg) {
 }
 
 async function home(filter) {
-    const classes = [{ type_id: "qilu", type_name: '齐鲁' },{ type_id: "jinan", type_name: '济南' },{ type_id: "qingdao", type_name: '青岛' },{ type_id: "12", type_name: '526' }];
+    const classes = [{ type_id: "qilu", type_name: '齐鲁' },{ type_id: "jinan", type_name: '济南' },{ type_id: "qingdao", type_name: '青岛' },{ type_id: "12", type_name: '5260' }];
     const filterObj = {};
     return JSON.stringify({
         class: _.map(classes, (cls) => {
@@ -172,7 +173,7 @@ async function play(flag, id, flags) {
     // let playUrl ='https://clivealone302.iqilu.com/291/cf348386147f4f5da17e4b3bc937bb63/playlist.m3u8?auth=06c0006852a7672f311c7535980a5194&timestamp=1727438974992'   
     const headers = {
         Referer: referer_s[index_1],
-        "User-Agent": IOS_UA
+        "User-Agent": Agent[index_1]
     };
     return JSON.stringify({
         parse: 0,
