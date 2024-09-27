6067c078741145de444f44e31514b5566556dab1
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-27 14:30:04 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -7,6 +7,7 @@ let siteType = 0;
 const IOS_UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
 const PC_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36";
 
+
 async function request(reqUrl, referer, mth, data, hd) {
     const headers = {
         "User-Agent": IOS_UA,
