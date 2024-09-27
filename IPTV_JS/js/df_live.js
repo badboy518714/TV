9454567cb67b55cd485e0e7ce94b6046dd13f1e1
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-27 17:52:40 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -18,7 +18,7 @@ async function request(reqUrl, referer, mth, data, hd) {
     let res = await req(reqUrl, {
         method: mth || "get",
         headers: headers,
-        data: data,
+        body: data,
         // postType: ""
         postType: mth === "post" ? "form" : "",
     });
@@ -52,7 +52,7 @@ async function init(cfg) {
 }
 
 async function home(filter) {
-    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: '15' }];
+    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: '85' }];
     const filterObj = {};
     return JSON.stringify({
         class: _.map(classes, (cls) => {
