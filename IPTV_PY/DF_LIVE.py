c945947467bdd3f9c3819fbc5575dc0d57875f40
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-28 06:12:26 +0800

    Update DF_LIVE.py

--- IPTV_PY/DF_LIVE.py
+++ IPTV_PY/DF_LIVE.py
@@ -96,7 +96,7 @@ def get_qingdao():
     cc = 'http://hlspull.qtv.com.cn/gwepn2sr/channel/4979ca520e89c36e8d6aa0d0a043e185/1.m3u8'
     for channel, radio_url in channel_radios.items():
         headers_["Referer"] = 'http://www.qtv.com.cn/live/radio/'
-        res = requests.get(radio_url, headers=headers_)
+        res = requests.get(radio_url, headers=headers_, verify=False)
         print(res.text)
         channelName = re.search(r"channelName: '([^']*)',", res.text).group(1)
         # print(channelName)
