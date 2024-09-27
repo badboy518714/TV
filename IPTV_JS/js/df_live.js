970a8c4551aa458131c03e9568496a5f071f9d97
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-27 15:59:26 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -52,7 +52,7 @@ async function init(cfg) {
 }
 
 async function home(filter) {
-    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: '45' }];
+    const classes = [{ type_id: "", type_name: '看电视' },{ type_id: "radio", type_name: '听广播' },{ type_id: "3", type_name: '1245' }];
     const filterObj = {};
     return JSON.stringify({
         class: _.map(classes, (cls) => {
@@ -78,7 +78,7 @@ async function category(tid, pg, filter, extend) {
         let _data = {'t': 1727406632331, 's': '01023c05067b65a46e918d51f7613a3d', 'data': 'r92+auLPIZZLbYQxhFq52A3bKeqbzL6b4aREFW4l7G0='}
         let data = _data["data"];
         let url = "https://feiying.litenews.cn/api/v1/auth/exchange?t=" + _data["t"] + "&s=" + _data["s"];
-        const res = await request(url, '', 'post', { body: data } );
+        const res = await request(url, '', 'post', _data );
         let ss;
         if(res) {ss = '111' }
         else { ss = '000' }
