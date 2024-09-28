09c026d6a0ea0c58fd8da76ba0e29792206c1081
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-28 15:38:43 +0800

    Update df_live.js

--- IPTV_JS/js/df_live.js
+++ IPTV_JS/js/df_live.js
@@ -13,7 +13,7 @@ const PIC_JN = ["https://quehuaobs.ijntv.cn/58CE1F28EF5E42F6/FABU_YUNSHI/388B909
 const ID_QD = ['QTV-1', 'QTV-2', 'QTV-3', 'QTV-4', 'QTV-5', 'QTV-6', '新闻综合广播', '经济广播', '文艺广播', '交通广播', '音体广播', '故事广播']
 const PIC_QD = [ "https://pic.rmb.bdstatic.com/bjh/240725/news/518b9f4f30e5ab4d84ee825c087d475d657.jpeg","https://photo.tuchong.com/17981284/f/744227682.jpg","https://qiniu-cdn0.jinxidao.com/group1/M00/09/9B/ooYBAFmyS4OAdU-MAALMB5guu1E493.jpg","https://travel.qingdaonews.com/gb/images/2020-02/13/ad403c27-097c-47bf-a1f4-de166fe3f083.jpg","https://p3.itc.cn/q_70/images03/20230304/5952c1795c4944e492ac67b413f4b97c.jpeg","https://bkimg.cdn.bcebos.com/pic/8b82b9014a90f603738df077aa5da41bb051f81947f9","https://mp-b77cdf68-e470-49bf-98e7-01d12ec29f28.cdn.bspapp.com/staic-img/tv-redio-back.jpg"]
 const ID_WH = ["新闻综合", "都市生活", "海洋频道", "新闻广播", "交通广播", "音乐广播"]
-const PIC_QD = [ "https://se-feed-bucket.bj.bcebos.com/map_x_day_sup/110ef30f33f46568ce7f2346878e92dd-s4.jpg", "https://www.weihai.gov.cn/picture/0/1903071456299066058.jpg", "https://q9.itc.cn/images01/20240409/0e64040d222641d992a8ff6b4dd9800c.jpeg", "http://lv.weihai.tv/files/images/2018-11/06103256681.jpg"]
+const PIC_WH = [ "https://se-feed-bucket.bj.bcebos.com/map_x_day_sup/110ef30f33f46568ce7f2346878e92dd-s4.jpg", "https://www.weihai.gov.cn/picture/0/1903071456299066058.jpg", "https://q9.itc.cn/images01/20240409/0e64040d222641d992a8ff6b4dd9800c.jpeg", "http://lv.weihai.tv/files/images/2018-11/06103256681.jpg"]
 let json_data;
 let jn_infos = {};
 let qd_infos = {};
