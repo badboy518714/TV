7ece84c11c5df8314f5914bf12594b3f5470aeae
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-10-01 18:00:25 +0800

    Add files via upload

---
+++ IPTV_JS/lib/来点看播.js
@@ -0,0 +1,31 @@
+var rule = {
+            title: '来点看播',
+            host: 'https://lkvod.me',
+            // homeUrl:'/',
+            url: '/show/fyclass--------fypage---.html',
+            searchUrl: '/nk/-------------.html?wd=**',
+            searchable: 2,//是否启用全局搜索,
+            quickSearch: 0,//是否启用快速搜索,
+            filterable: 0,//是否启用分类筛选,
+            headers: {//网站的请求头,完整支持所有的,常带ua和cookies
+                'User-Agent': 'MOBILE_UA',
+                // "Cookie": "searchneed=ok"
+            },
+            class_parse: '.wap-show0&&ul&&li;a&&Text;a&&href;/.*/(.*?).html',
+            //cate_exclude: '解析|动态',
+            play_parse: true,
+            lazy: '',
+            limit: 6,
+            推荐: 'body&&.fadeInUp;.diy-center&&.public-list-box;a&&title;img&&data-src;.ft2&&Text;a&&href',
+            double: true, // 推荐内容是否双层定位
+            一级: 'body&&.fadeInUp&&.diy-center&&.public-list-box;a&&title;img&&data-src;.ft2&&Text;a&&href',
+            二级: {
+                "title": "h3&&Text;.slide-info:eq(0)&&Text",
+                "img": ".detail-pic&&img&&data-src",
+                "desc": ".slide-info:eq(1)&&Text;.module-info-item:eq(2)&&Text,.module-info-item:eq(3)&&Text,.module-info-item:eq(4)&&Text",
+                "content": ".cor3&&Text",
+                "tabs": ".br&&.swiper-slide",
+                "lists": ".anthology-list-play.size:eq(#id) a"
+            },
+            搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
+        }
