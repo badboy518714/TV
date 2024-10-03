aa9d18b32eafc03f8bbeddaadb38ef512af38277
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-10-04 04:04:53 +0800

    Update 福音影视.js

--- IPTV_JS/lib/福音影视.js
+++ IPTV_JS/lib/福音影视.js
@@ -4,19 +4,28 @@ var rule = {
     homeUrl:'',
     searchUrl:'movies/search/**/1',
     searchable:1,
-    quickSearch:0,
-    //filterable:1,
-    multi:1,
+    quickSearch:1,
+    filterable:1,
+    //multi:1,
     // 分类链接fypage参数支持1个()表达式
-    url:'/content/fyclass/page/fypage/index.html',//fyfilter
-    //filter_url:'{{fl.style}page/fypage',
+    url:'/content/fyfilter/page/fypage/index.html',//fyfilter
+    filter_url: '{{fl.cateId}}',
+    //filterable:1,//是否启用分类筛选,
     headers:{'User-Agent': 'PC_UA'},
     timeout:15000,
     class_name:'热播排行&最新发布&最近更新&主日讲道&全部视频&福音慕道&福音证道&婚姻家庭&赞美诗歌&福音见证&福音视频&圣乐崇拜&初信造就&福音动漫&神学课程',
-    class_url:'hot&publish&latest&category/id/205&all&category/id/133&category/id/22&category/id/34&category/id/24&category/id/42&category/id/21&category/id/26&category/id/25&category/id/23&category/id/290',//
+    class_url:'hot&publish&latest&主日&all&慕道&证道&家庭&诗歌&见证&视频&崇拜&初信&动漫&神学',
 	
-    //filter:{},
-    limit:20,
+    filter: {
+				"主日": [{"key":"cateId","name":"牧者讲员","value":[{"n":"矽谷基督徒聚会","v":"category/id/237"},{"n":"台北基督之家","v":"category/id/313"},{"n":"橄榄山基督教会","v":"category/id/238"},{"n":"橙县中华福音教会","v":"category/id/240"},{"n":"贝郡基督徒证主教会","v":"category/id/286"},{"n":"东区教会信息","v":"category/id/243"},{"n":"基督之五家信息","v":"category/id/244"},{"n":"凤凰城基督教会","v":"category/id/251"},{"n":"恩城基督徒证主教会","v":"category/id/327"}]}, {'key': 'cateId', 'name': '年份', 'value': [{'n': '2024', 'v': 'category/id/341'}, {'n': '2023', 'v': 'category/id/333'}, {'n': '2022', 'v': 'category/id/328'}, {'n': '2021', 'v': 'category/id/326'}, {'n': '2020', 'v': 'category/id/319'}, {'n': '2019', 'v': 'category/id/318'}, {'n': '2018', 'v': 'category/id/316'}, {'n': '2017', 'v': 'category/id/299'}, {'n': '2016', 'v': 'category/id/294'}, {'n': '2015', 'v': 'category/id/278'}, {'n': '2014', 'v': 'category/id/262'}, {'n': '2013', 'v': 'category/id/250'}, {'n': '2012', 'v': 'category/id/249'}, {'n': '2011', 'v': 'category/id/248'}, {'n': '2010', 'v': 'category/id/247'}, {'n': '2009', 'v': 'category/id/246'}, {'n': '2008', 'v': 'category/id/245'}]}
+]
+
+			  },
+    filter_def:{
+        hot:{cateId:'hot'},publish:{cateId:'publish'},latest:{cateId:'latest'},all:{cateId:'all'},主日:{cateId:'category/id/205'},慕道:{cateId:'category/id/133'},证道:{cateId:'category/id/22'},家庭:{cateId:'category/id/34'},
+			诗歌:{cateId:'category/id/24'},见证:{cateId:'category/id/42'},视频:{cateId:'category/id/21'},崇拜:{cateId:'category/id/26'},初信:{cateId:'category/id/25'},动漫:{cateId:'category/id/23'},神学:{cateId:'category/id/290'}
+     },
+    limit:6,
     play_parse:true,
     //play_json:1, 
     // 手动调用解析请求json的url,此lazy不方便     //'https://www.ifuyin.net/html/'
@@ -31,8 +40,10 @@ var rule = {
 				parse: 0
 			 }
 		  `,
-    // 推荐:'.list_item;img&&alt;img&&src;a&&Text;a&&data-float',
+    推荐:'.albums;.albums&&.album-item;.c-primary&&Text;.loader&&style;.c-thirdary&&Text;a&&href',
+    double: true, // 推荐内容是否双层定位
     一级:`js:
+			print(input);
         var d = [];
         pdfh = jsp.pdfh;
         pdfa = jsp.pdfa;
