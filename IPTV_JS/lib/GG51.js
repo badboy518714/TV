06a8593c105a06fc6f1af2c68344a4fa62c61cd7
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-10-08 17:57:28 +0800

    Update GG51.js

--- IPTV_JS/lib/GG51.js
+++ IPTV_JS/lib/GG51.js
@@ -1,8 +1,8 @@
 var rule = {
             title: '精品资源',
-            //host: 'https://gg51-001.xyz',
-            host: "https://jgtq.gg51-lieb401.vip",
-            //hostJs: 'print(HOST); let html;  for(let i=0;i<20;i++){ html = decodeURIComponent(request(HOST,{headers:{"User-Agent":PC_UA}})); if(html) break; }  let match_s = html.match(/(gg51-[^.]*.vip)/gi);print(match_s); let host_list = []; for(let i in match_s){ print(i); print(match_s[i]);host_list.push("https://jgtq." + match_s[i]); host_list.push("https://d90." + match_s[i]); } print(host_list);let flag = 0; for(let i in host_list){ for( let j=0;j<5;j++){ html = request(host_list[i]); if(html){ print("获取--HOST--成功"); flag = 1 ;HOST = host_list[i]; break; }else{  print("获取--HOST--失败"); }}if(flag) break;} print(HOST);',
+            host: 'https://gg51-001.xyz',
+            //host: "https://jgtq.gg51-lieb401.vip",
+            hostJs: 'print(HOST); let html;  for(let i=0;i<20;i++){ html = decodeURIComponent(request(HOST,{headers:{"User-Agent":PC_UA}})); if(html) break; }  let match_s = html.match(/(gg51-[^.]*.vip)/gi);print(match_s); let host_list = []; for(let i in match_s){ print(i); print(match_s[i]);host_list.push("https://jgtq." + match_s[i]); host_list.push("https://d90." + match_s[i]); } print(host_list);let flag = 0; for(let i in host_list){ for( let j=0;j<5;j++){ html = request(host_list[i]); if(html){ print("获取--HOST--成功"); flag = 1 ;HOST = host_list[i]; break; }else{  print("获取--HOST--失败"); }}if(flag) break;} print(HOST);',
             timeout:3000, 
             url: '/category/fyclass/fypage/',
             searchUrl: '/search/**/fypage/',
@@ -46,16 +46,16 @@ var rule = {
 						let html;
 						let d = [];
 						function secondsToHMS(seconds) {
-							seconds = parseInt(seconds)
+							seconds = parseInt(seconds);
   							let hours = Math.floor(seconds / 3600);
   							let minutes = Math.floor((seconds % 3600) / 60);
-							//print(hours, minutes, minutes)
+							print(hours, minutes, minutes)
   							seconds = seconds % 60;
 
   							return hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
 						}
 						for(let i=0;i<5;i++){
-							let html = request(input);
+							html = request(input);
 							if(html) break;
 						}
 						html = decodeURIComponent(html).replace(/\\n/gi, "").replace(/ /gi, "");
