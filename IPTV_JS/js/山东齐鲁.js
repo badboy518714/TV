e18e665a19cb8ce814b8012af736381cd0680869
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2025-01-02 19:12:02 +0800

    Update 山东齐鲁.js

--- IPTV_JS/js/山东齐鲁.js
+++ IPTV_JS/js/山东齐鲁.js
@@ -7,50 +7,56 @@ var rule = {
     searchable:0,
     quickSearch:1,
     filterable:0,
+    //multi:1,
     // 分类链接fypage参数支持1个()表达式
     url:'/fyclass/',
-    headers:{'User-Agent': 'MOBILE_UA','referer': 'https://v.iqilu.com'},
+    headers:{'User-Agent': 'PC_UA','referer': 'https://v.iqilu.com'},
     timeout:5000,
     class_name:'山东频道',
     class_url:'shandong',
     pagecount:{"shandong":1},
     limit:5,
     play_parse:true,
+    //play_json:1,
+    // 手动调用解析请求json的url,此lazy不方便     //'https://www.ifuyin.net/html/'
     lazy: `js:
-		input = {
-			jx: 1,
-			url: input,
-			parse: 1,
-			header: JSON.stringify({
-				'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0',
-				'referer': 'https://v.iqilu.com'
-			}),
-		}
-	 `,
+			input = {
+				jx: 1,
+				url: input,
+				//playUrl: input,
+				parse: 0,
+				header: JSON.stringify({
+					'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0',
+					'referer': 'https://v.iqilu.com'
+				}),
+			}
+			`,
     推荐:'',
     double: true, // 推荐内容是否双层定位
     一级:`js:
+			print(input)
 			let d = [];
 			let pdfh = jsp.pdfh;
 			let pdfa = jsp.pdfa;
 			let pd = jsp.pd;
 
 			let html = request('https://v.iqilu.com/'); 
-			
-			let items = pdfa(html, '.container&&dl');
+			//print(html)
+			let items = pdfa(html, 'body&&ul.wrapper&&li');
 
 			print(items)
 			let pic_url;
 			for(let i=0;i<items.length;i++){
+				if(i%10==0){ continue; }
 				let title;let pic_url;let url;
 				title = pd(items[i], 'a&&title');
-				pic_url =  pd(items[i], 'img&&src')
+				//pic_url =  pd(items[i], 'img&&src')
 				url = pd(items[i], 'a&&href')
-
+				print(title)
            d.push({
                title: title,
                desc: '', 
-               pic_url: pic_url,
+               pic_url: '',
                url: url
            })
         };
